bef7bd8b4cc5de83b141518f201e31dc8a7fa717
Aryaman saxena <arya1122002@gmail.com>
2024-06-22 15:47:32 +0530

    react native vector icons

--- rn/screens/home/index.jsx
+++ rn/screens/home/index.jsx
@@ -2,10 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import AddNewPost from '../addNewPost'
 
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
+
 const Home = () => {
   return (
     <View>
       <Text>This is Home</Text>
+      <EvilIcons name="search" size={50}/>
       <AddNewPost />
     </View>
   )
