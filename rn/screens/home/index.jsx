48631d73a9ea974ef32c6b53d2d1d4e1edc080df
Samarth Jain <samarthj0612@gmail.com>
2024-06-22 19:21:59 +0530

    Main home screen - Home feeds

--- rn/screens/home/index.jsx
+++ rn/screens/home/index.jsx
@@ -1,15 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import AddNewPost from '../addNewPost'
 
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
+import Post from '../../components/Post'
+import { posts, users } from '../../assets/data'
 
 const Home = () => {
   return (
-    <View>
-      <Text>This is Home</Text>
-      <EvilIcons name="search" size={50}/>
-      <AddNewPost />
+    <View style={{ flex: 1, backgroundColor: '#020202' }}>
+
+      <ScrollView>
+
+        {
+          posts.map((post, i) => {
+            return <Post name={users[post.createdBy].name} username={users[post.createdBy].username} text={post.text} media={post.media} />
+          })
+        }
+
+      </ScrollView>
+      
     </View>
   )
 }
